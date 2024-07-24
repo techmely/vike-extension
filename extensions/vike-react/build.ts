72386578f31ddbf62b1834a17c27950f2337f103
Harry Tran <nhattq.coding@gmail.com>
2024-07-24 06:49:58 +0000

    fix build esm issues

--- extensions/vike-react/build.ts
+++ extensions/vike-react/build.ts
@@ -1,18 +1,17 @@
 import { execSync } from "node:child_process";
-import { glob } from "glob";
-
-const tsFiles = await glob("src/**/*.ts*");
 
 execSync("rm -rf dist");
-await Bun.build({
-  entrypoints: tsFiles,
-  outdir: "dist",
-  target: "node",
-  format: "esm",
-  splitting: true,
-  sourcemap: "inline",
-  external: ["*"],
-});
+execSync("bun run build.tsup");
+
+// await Bun.build({
+//   entrypoints: tsFiles,
+//   outdir: "dist",
+//   target: "node",
+//   format: "esm",
+//   splitting: true,
+//   sourcemap: "inline",
+//   external: ["*"],
+// });
 execSync("bun run tsc --emitDeclarationOnly --outDir dist");
 execSync("mv dist/src/* dist");
 execSync("cp src/typing.d.ts dist");
